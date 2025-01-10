4578e6b2faf94c58cb9e713a3afb5d5b5a58a90c
Talal <talal.codes@gmail.com>
2025-01-09 23:12:05 -0600

    pushing and commiting chapter_4

--- Swift_App/Chapter_4.swift
+++ Swift_App/Chapter_4.swift
@@ -1,8 +1,14 @@
 //
-//  Chapter_4.swift
+//  Advance Control Flow.
 //  Swift_App
 //
 //  Created by Muhammad Talal on 2025-01-09.
 //
 
 import Foundation
+
+//In this chapter we learn about for loops and switch st.
+
+for i in 0...5 {
+    print(i)
+}
